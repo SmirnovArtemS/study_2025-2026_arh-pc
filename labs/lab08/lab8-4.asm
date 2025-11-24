3e908ebd804b2fab490c59b54b2d70fdcc300bea
ArtemSmirnovS <1032252364@pfur.ru>
2025-11-24 16:26:22 +0300

    feat(main): make course structure

---
+++ labs/lab08/lab8-4.asm
@@ -0,0 +1,31 @@
+%include 'in_out.asm'
+SECTION .data
+msg db "Результат: ",0
+func db "Функция: f(x) = 4x + 3",0
+SECTION .text
+global _start
+
+_start:
+pop ecx
+pop edx
+sub ecx,1
+mov esi, 0
+
+next:
+cmp ecx,0h
+jz _end
+pop eax
+call atoi
+imul eax, 4
+add eax, 3
+add esi,eax
+loop next
+
+_end:
+mov eax, func
+call sprintLF
+mov eax, msg
+call sprint
+mov eax, esi
+call iprintLF
+call quit
